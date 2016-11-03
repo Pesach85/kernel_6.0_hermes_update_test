0d2c4251447379c81487c31eb9638adc1a648247
Pesach85 <pasqualelombardi@live.it>
2016-11-03 13:24:18 +0100

    Update to kernel.org Linux 3.10.80

--- kernel-3.10/drivers/mtd/ubi/wl.c
+++ kernel-3.10/drivers/mtd/ubi/wl.c
@@ -1014,8 +1014,8 @@ int ubi_wl_put_fm_peb(struct ubi_device *ubi, struct ubi_wl_entry *fm_e,
 static int wear_leveling_worker(struct ubi_device *ubi, struct ubi_work *wrk,
 				int cancel)
 {
-	int erase_e2=1, err, scrubbing = 0, torture = 0, protect = 0, erroneous = 0;
-	int vol_id = -1, uninitialized_var(lnum);
+	int err, scrubbing = 0, torture = 0, protect = 0, erroneous = 0;
+	int vol_id = -1, lnum = -1;
 #ifdef CONFIG_MTD_UBI_FASTMAP
 	int anchor = wrk->anchor;
 #endif
